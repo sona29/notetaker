1b54193e7d8777459e15528e337ea611fc2118a2
Sona Shrestha <sona.shrestha004@gmail.com>
2021-05-14 13:52:48 +1000

    adding routing files

--- routing/api-routes.js
+++ routing/api-routes.js
@@ -1,1 +1,14 @@
-let note
+
+const path = require("path");
+
+var noteData = require('../db/db.json');
+
+module.exports = function (app){
+    app.get('/api/notes', (req, res) => res.json(noteData));
+
+
+    app.post("/api/notes", (req, res) =>{
+        
+
+    });
+};
