a6a25da56a02e31720316d26621f27c266a43fdf
Sona Shrestha <sona.shrestha004@gmail.com>
2021-05-14 14:41:46 +1000

    adding function to add new note

--- routing/api-routes.js
+++ routing/api-routes.js
@@ -1,7 +1,7 @@
 
 const path = require("path");
-
-var noteData = require('../db/db.json');
+let noteData = require('../db/db.json');
+const fs = require("fs");
 
 module.exports = function (app){
     app.get('/api/notes', (req, res) => res.json(noteData));
@@ -9,6 +9,15 @@ module.exports = function (app){
 
     app.post("/api/notes", (req, res) =>{
         
-        noteData.push(req.body);
+        let newNote = req.body;
+        // console.log(newNote);
+
+        // reading json file
+        let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+        
+        //adding new note
+        data.push(newNote);
+        fs.writeFileSync('./db/db.json', JSON.stringify(data));
+        res.json(data);
     });
 };
