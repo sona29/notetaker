dc530d6ac767e495618bd0c6dc5f67fa583f6296
Sona Shrestha <sona.shrestha004@gmail.com>
2021-05-15 14:07:20 +1000

    adding get function

--- routing/api-routes.js
+++ routing/api-routes.js
@@ -4,7 +4,13 @@ let noteData = require('../db/db.json');
 const fs = require("fs");
 
 module.exports = function (app){
-    app.get('/api/notes', (req, res) => res.json(noteData));
+    app.get('/api/notes', (req, res) => {
+        // res.json(noteData);
+        let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+        res.json(data);
+         
+        
+    });
 
 
     app.post("/api/notes", (req, res) =>{
@@ -19,5 +25,12 @@ module.exports = function (app){
         data.push(newNote);
         fs.writeFileSync('./db/db.json', JSON.stringify(data));
         res.json(data);
+
+        app.get('/api/notes', (req, res) => {
+            console.log(noteData);
+            res.json(noteData);
+             
+            
+        });
     });
 };
