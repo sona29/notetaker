c9a5ba3637a9a4964e7899c8ca0da4538f35522c
Sona Shrestha <sona.shrestha004@gmail.com>
2021-05-15 22:38:10 +1000

    adding delete function in route

--- routing/api-routes.js
+++ routing/api-routes.js
@@ -1,4 +1,3 @@
-
 const path = require("path");
 let noteData = require('../db/db.json');
 const fs = require("fs");
@@ -8,21 +7,22 @@ const random = require('random')
 
 
 module.exports = function (app){
+
+    //for gettting data
     app.get('/api/notes', (req, res) => {
         
         let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-        res.json(data);
-         
+        res.json(data);         
         
     });
 
-
+    //for posting data
     app.post("/api/notes", (req, res) =>{
         
         let newNote = req.body;    
 
         // generating random number
-        newNote.id = random.int(0, 100);
+        newNote.id = random.int(0, 1000);
         console.log("new note with id"+ newNote);
 
         // reading json file
@@ -39,5 +39,26 @@ module.exports = function (app){
              
             
         });
+    });
+
+    //for deleting data
+    app.delete('/api/notes/:id', (req, res) => {
+        let id = req.params.id;
+        console.log('delete id  ' + id);
+
+        let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+        // console.log(res.json(data)); 
+        for(let i =0;i<data.length;i++){
+            // console.log(i);
+            let idNote = data[i];
+            if (idNote.id == id){
+
+                // console.log('if');
+                data.splice(i, 1);
+                console.log(data);
+                fs.writeFileSync('./db/db.json', JSON.stringify(data));
+            }
+
+        }
     });
 };
