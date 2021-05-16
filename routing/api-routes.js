25011d7300dcec7e045b84e10853ab9acc9c7492
Sona Shrestha <sona.shrestha004@gmail.com>
2021-05-16 14:02:10 +1000

    adding comments

--- routing/api-routes.js
+++ routing/api-routes.js
@@ -11,6 +11,7 @@ module.exports = function (app){
     //for gettting data
     app.get('/api/notes', (req, res) => {
         
+        // to read file
         let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
         res.json(data);         
         
@@ -22,19 +23,19 @@ module.exports = function (app){
         let newNote = req.body;    
 
         // generating random number
-        newNote.id = random.int(0, 10000);
-        console.log("new note with id"+ newNote);
+        newNote.id = random.int(0, 10000);        
 
         // reading json file
         let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
         
         //adding new note
         data.push(newNote);
+        // to write to files 
         fs.writeFileSync('./db/db.json', JSON.stringify(data));
         res.json(data);
 
         app.get('/api/notes', (req, res) => {
-            console.log(noteData);
+            
             res.json(noteData);
              
             
@@ -43,21 +44,19 @@ module.exports = function (app){
 
     //for deleting data
     app.delete('/api/notes/:id', (req, res) => {
+        //selected note id
         let id = req.params.id;
-        console.log('delete id  ' + id);
-
         let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
        
         for(let i =0;i<data.length;i++){
            
             let idNote = data[i];
 
-            //deleting if 
+            //deleting the note that matches the selected note id
             if (idNote.id == id){
-
                 // console.log('if');
                 data.splice(i, 1);
-                console.log(data);
+                // to write to files 
                 fs.writeFileSync('./db/db.json', JSON.stringify(data));
                 res.json(data);
 
